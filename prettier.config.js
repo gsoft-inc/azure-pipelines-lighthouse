68a1ef12c1341f17101e1b1ffeb7cca062f49477
Anthony Simmon <simmon.anthony@gmail.com>
2020-02-06 22:39:24 -0500

    Added prettier and husky

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'none',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 140,
+  arrowParens: 'avoid',
+  bracketSpacing: true
+};
