c9174e70cb12fa32ae88109227374cb904c5d4b1
Anthony Simmon <simmon.anthony@gmail.com>
2023-03-12 01:35:43 -0500

    Added timeout

--- task/tests/integration/L0.spec.ts
+++ task/tests/integration/L0.spec.ts
@@ -8,6 +8,8 @@ import {MockTestRunner} from 'azure-pipelines-task-lib/mock-test';
 // Inspiration from MS built-in tasks
 // https://github.com/microsoft/azure-pipelines-tasks/blob/1d86805603c6aaa07b29bf5523beaf6d9977824a/Tasks/GulpV1/Tests/L0.ts
 describe('LighthouseV1 suite', function () {
+  this.timeout(20000);
+
   it('L0IntegrationTest1', done => {
     const tr = new MockTestRunner(path.join(__dirname, 'L0IntegrationTest1.js'));
 
