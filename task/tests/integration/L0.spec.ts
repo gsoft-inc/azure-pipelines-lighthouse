770ca6b10ab065bec1481a7986685823f901b33b
Anthony Simmon <simmon.anthony@gmail.com>
2023-03-12 13:32:28 -0400

    Provide task.json path to test

--- task/tests/integration/L0.spec.ts
+++ task/tests/integration/L0.spec.ts
@@ -8,12 +8,18 @@ import {MockTestRunner} from 'azure-pipelines-task-lib/mock-test';
 // Inspiration from MS built-in tasks
 // https://github.com/microsoft/azure-pipelines-tasks/blob/1d86805603c6aaa07b29bf5523beaf6d9977824a/Tasks/GulpV1/Tests/L0.ts
 describe('LighthouseV1 suite', function () {
-  this.timeout(20000);
+  // The timeout is required because azure-pipelines-task-lib actually downloads Node.js and it takes some time
+  // https://github.com/microsoft/azure-pipelines-task-lib/blob/149de35abb4ee930cf512c3d88d9178f277fb3fe/node/mock-test.ts#L227
+  this.timeout('20s');
 
   it('L0IntegrationTest1', done => {
-    const tr = new MockTestRunner(path.join(__dirname, 'L0IntegrationTest1.js'));
+    const testPath = path.join(__dirname, 'L0IntegrationTest1.js');
+    const taskJsonPath = path.join(__dirname, '..', '..', 'src', 'task.json');
+    const tr = new MockTestRunner(testPath, taskJsonPath);
 
-    tr.run();
+    // As of today (2023-03-12), azure-pipelines-task-lib testing framework doesn't support Node 18
+    // https://github.com/microsoft/azure-pipelines-task-lib/blob/149de35abb4ee930cf512c3d88d9178f277fb3fe/node/mock-test.ts#L156
+    tr.run(16);
 
     assert(tr.invokedToolCount === 1, 'should have only run lighthouse');
     assert(tr.succeeded, 'task should have succeeded');
